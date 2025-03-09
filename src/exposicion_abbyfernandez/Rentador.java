064ba4c05aad690a5962796ead157684aaa19184
abbyfer <abbyf052@gmail.com>
2025-03-09 14:30:01 -0600

    clase rentador

--- src/exposicion_abbyfernandez/Rentador.java
+++ src/exposicion_abbyfernandez/Rentador.java
@@ -9,5 +9,65 @@ package exposicion_abbyfernandez;
  * @author afern
  */
 public class Rentador {
+    private String nombre;//nombre de quien renta el libro
+    private String apellido;
+    private String DNI;//numero de id
+    private int edad;//edad del rentador
+    private int rentados;//cantidad de libros que ha rentado
+
+    //constructor
+    public Rentador(String nombre, String apellido, String DNI, int edad, int rentados) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.DNI = DNI;
+        this.edad = edad;
+        this.rentados = rentados;
+    }
+    //getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getRentados() {
+        return rentados;
+    }
+
+    public void setRentados(int rentados) {
+        this.rentados = rentados;
+    }
+
+    @Override
+    public String toString() {
+        return "Rentador{" + "nombre=" + nombre + ", apellido=" + apellido + ", DNI=" + DNI + ", edad=" + edad + ", rentados=" + rentados + '}';
+    }
+    
     
 }
